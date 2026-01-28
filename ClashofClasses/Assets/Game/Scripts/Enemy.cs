827cd45fa76951397ae1ef7f4f0679f645e44ea0
Harvey <Harvey.D.Smith@students.ittralee.ie>
2026-01-28 19:11:33 +0000

    Worked on attacking, and fixed some issues with collision (now using triggers), added sprites and slight animation for attacking. Finished movement test scene Signed-off-by: Harvey <Harvey.D.Smith@students.ittralee.ie>

--- ClashofClasses/Assets/Game/Scripts/Enemy.cs
+++ ClashofClasses/Assets/Game/Scripts/Enemy.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +13,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = transform.position - player.position;
+        direction.y = 0f; 
+
+        if (direction.sqrMagnitude > 0.001f)
+            transform.rotation = Quaternion.LookRotation(direction);
+
     }
 
     void OnCollisionEnter(Collision collision)
---
+++ ClashofClasses/Assets/Game/Scripts/TestDummy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestDummy : MonoBehaviour
+{
+    public Transform player;
+    Renderer changeColor;
+    Color defaultColor;
+    //bool spin;
+    public Rigidbody rb;
+    float spinForce = 2000f;
+    void Start()
+    {
+        changeColor = GetComponent<Renderer>();
+        defaultColor = changeColor.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = transform.position - player.position;
+        direction.y = 0f;
+
+        if (direction.sqrMagnitude > 0.001f)
+            transform.rotation = Quaternion.LookRotation(direction);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Attack"))
+        {
+          
+            StartCoroutine(FlashRed());
+            
+            
+        }
+    }
+
+    IEnumerator FlashRed()
+    {
+        changeColor.material.color = Color.red;
+       // spin = true;
+        yield return new WaitForSeconds(0.5f);
+        changeColor.material.color = defaultColor;
+        //spin = false;
+    }
+}
